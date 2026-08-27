898ea7cfe466e5095826a131c8a23621abcdf491
Jemal Khachidze <jkhachidze92@gmail.com>
2026-08-27 17:50:40 +0200

    feat(auctions): create Auction Entitiy

--- src/auctions/entities/auction.entity.ts
+++ src/auctions/entities/auction.entity.ts
@@ -1,1 +1,30 @@
-export class Auction {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn
+} from 'typeorm';
+
+@Entity()
+export class Auction {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column()
+  title!:string;
+
+  @Column()
+  description!: string;
+
+  @Column()
+  startPrice!: number;
+
+  @Column()
+  currentPrice!: number;
+  // TODO: replace with real DB relation once seller/user table is present
+  @Column()
+  seller!: string;
+
+  @CreateDateColumn()
+  endDate!: Date;
+}
