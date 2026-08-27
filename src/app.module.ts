3c71d0c0cf07cdab3dd7fcd339e74eea5be82e8b
manuasdf <git@manuasdf.dev>
2026-08-27 18:30:31 +0200

    feat[app]: Add imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,16 @@ import { ConfigModule } from '@nestjs/config';
 import { AppDataSource } from './db/data-source';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuctionsModule } from './auctions/auctions.module';
+import { OfferModule } from './offer/offer.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot(AppDataSource.options),
     AuctionsModule,
+    OfferModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
